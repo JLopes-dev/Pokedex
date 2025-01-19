ac68f6fa6a296327651db70418f0c966a31ee3e2
JLopes-dev <joaopedrolopessardinha@gmail.com>
2025-01-18 23:31:07 -0300

    add logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraPesquisaComponent } from './components/barra-pesquisa/barra-pesquisa.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraPesquisaComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
