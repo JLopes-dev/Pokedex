c124388c62a95614591de8e0e958c8d8b28815ad
JLopes-dev <joaopedrolopessardinha@gmail.com>
2025-01-19 20:52:57 -0300

    return names and images of the pokemons

--- src/app/components/pokemon-item/pokemon-item.component.ts
+++ src/app/components/pokemon-item/pokemon-item.component.ts
@@ -9,11 +9,19 @@ import { ApiService } from 'src/app/services/api.service';
 export class PokemonItemComponent implements OnInit {
 
   private listaPokemon: any[] = []
+  private allNames: string[] = []
+  private allPokeInfos: string[] = []
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.apiService.getAll().subscribe(data => {
       this.listaPokemon.push(...data.results)
+      this.listaPokemon.forEach(pokemon => this.allNames.push(pokemon.name))
+      this.listaPokemon.forEach(pokemon => this.apiService.getOnePokemon(pokemon.url)
+      .subscribe(data => {
+        this.allPokeInfos.push(data.sprites.back_default)
+      }))
+      console.log(this.allPokeInfos);
     })
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-interface arrayAllPokemons {
+interface AllPokemonsInterface {
   results: Array<any>
 }
 
@@ -15,8 +15,12 @@ export class ApiService {
   private urlBase: string = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
   constructor(private http: HttpClient) { }
 
-  getAll(): Observable<arrayAllPokemons> {
-    return this.http.get<arrayAllPokemons>(this.urlBase)
+  getAll(): Observable<AllPokemonsInterface> {
+    return this.http.get<AllPokemonsInterface>(this.urlBase)
+  }
+
+  getOnePokemon(url: string): Observable<any> {
+    return this.http.get<any>(url)
   }
 
 }
